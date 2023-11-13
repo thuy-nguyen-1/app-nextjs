7819e10d1eeca1d7d8388734477c72eabddb5bd9
Thuy Nguyen <thuy.nguyen1@tyme.com>
2023-11-13 13:46:29 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    const { isServer } = options;
+    if (!isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "app_nextjs",
+          remotes: {
+            // next1: `next1@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+          },
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./page": "./pages/index.tsx",
+          },
+          shared: {
+            react: {
+              requiredVersion: false,
+              singleton: true,
+            },
+          },
+          extraOptions: {
+            skipSharingNextInternals: true,
+          },
+        })
+      );
+    }
+    config.output.publicPath = process.env.DOMAIN_HOST;
+    return config;
+  },
+  assetPrefix: process.env.DOMAIN_HOST,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
